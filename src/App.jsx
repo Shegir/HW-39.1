3c52e5773aa3a9e807dcca723dfea7b093b80dc9
Serhii Kozub <sergey666kozub@gmail.com>
2025-11-25 23:21:15 +0200

    HW39

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from './components/Counter';
+import Greeting from './components/Greeting';
+import ClassCounter from './components/ClassCounter';
+
+export default function App() {
+    return (
+        <div>
+            <Greeting name="Serhii" />
+            <Counter />
+            <ClassCounter />
+        </div>
+    );
+}
---
+++ src/components/ClassCounter.js
@@ -0,0 +1,21 @@
+import { Component } from 'react';
+
+export default class ClassCounter extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { count: 0 };
+    }
+
+    increment = () => this.setState({ count: this.state.count + 1 });
+    decrement = () => this.setState({ count: this.state.count - 1 });
+
+    render() {
+        return (
+            <div>
+                <h2>Class Counter: {this.state.count}</h2>
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
+            </div>
+        );
+    }
+}
